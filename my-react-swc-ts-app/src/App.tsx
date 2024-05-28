6ccd336b07433eac55f78e2f94b850dfbb9ec163
Babajide Faruq Olami <115806631+Faruqbabajide@users.noreply.github.com>
2024-05-28 11:24:02 +0100

    (myfirst)

---
+++ my-react-swc-ts-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from 
+'./components/HelloWorld';
+const App: React.FC = () => {
+return (
+<div className=
+"pl-4 pr-4">
+<HelloWorld />
+</div>
+);
+}
+export default App;
